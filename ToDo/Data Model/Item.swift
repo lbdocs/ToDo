deba5f289180232ccbdc808e21358407aba57a13
LucieB <luciebaker.ubi@gmail.com>
2019-05-01 11:38:14 +0300

    Convert to MVC design pattern

---
+++ ToDo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDo
+//
+//  Created by Lucie Baker on 5/1/19.
+//  Copyright © 2019 Lucie Baker. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
